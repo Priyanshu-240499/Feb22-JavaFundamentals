66a5c14875aef4ee9e41049ddf39469bf1bf9110
Priyanshu-240499 <97544184+Priyanshu-240499@users.noreply.github.com>
2022-03-13 19:24:14 +0530

    Add files via upload

---
+++ March/week2/march10/March10Assignment.java
@@ -0,0 +1,17 @@
+public class March10Assignment {
+	  public static void validate(int age) {  
+	        if(age<18) {  
+	            throw new ArithmeticException("Person is not eligible to vote");    
+	        }  
+	        else {  
+	            System.out.println("Person is eligible to vote!!");  
+	        }  
+	    }  
+	    //main method  
+	    public static void main(String args[]){  
+	        //calling the function  
+	        validate(13);  
+	        System.out.println("rest of the code...");    
+	  }    
+
+}
