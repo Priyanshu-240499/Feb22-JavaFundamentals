9404c6870e59d4f57fbc44b8999b41bc66f4b95c
Priyanshu-240499 <97544184+Priyanshu-240499@users.noreply.github.com>
2022-03-20 15:10:45 +0530

    Add files via upload

---
+++ March/week 3/march18/Assignment2/Sports.java
@@ -0,0 +1,26 @@
+package march18Assignmentt2;
+
+public class Sports {
+	String getName() {
+    return "Generic Sports";
+  }
+
+  void getNumberOfTeamMembers() {
+    System.out.println("Each team has n players in " + getName());
+  }
+  public static void main(String[] args) {
+	    Sports c1 = new Sports();
+	    Soccer c2 = new Soccer();
+	    System.out.println(c1.getName());
+	    c1.getNumberOfTeamMembers();
+	    System.out.println(c2.getName());
+	    c2.getNumberOfTeamMembers();
+	  }
+}
+class Soccer extends Sports {
+
+	  @Override
+	  String getName() {
+	    return "Soccer Class";
+	  }
+	}
